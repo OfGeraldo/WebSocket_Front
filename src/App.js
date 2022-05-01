befed1be995dab455fa53af8e73837b736633cc7
Geraldo Junior <geraldojunioroficial@gmail.com>
2022-05-01 17:56:17 -0300

    Modificações

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import useWebSocket from 'react-use-websocket';
 
 function App() {
+
+  const [numero,setNumero] = useState(0);
+
+  const { lastJsonMessage } = useWebSocket('ws://localhost:4000', {
+    onOpen: () => console.log(`Connected to App WS`),
+    onMessage: () => {
+      if (lastJsonMessage) {
+        console.log(lastJsonMessage);
+        setNumero(lastJsonMessage.temp);
+      }
+    },
+    onError: (event) => { console.error(event); },
+    shouldReconnect: (closeEvent) => true,
+    reconnectInterval: 3000
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {numero}
       </header>
     </div>
   );
